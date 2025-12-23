ea0bfc47bfb8b1b9c761015b54d49424268318e4
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-12-22 19:53:42 -0500

    Cambio de nombre del proyecto

--- frontend/src/components/Logo.tsx
+++
@@ -1,7 +0,0 @@
-export const Logo = () => {
-    return (
-        <div className="flex justify-center ">
-            <img src="/logo.png" className="w-40"></img>
-        </div>
-    )
-}
--- frontend/src/types/index.ts
+++
@@ -1,2 +0,0 @@
-export { AuthSchema } from './AuthTypes';
-
