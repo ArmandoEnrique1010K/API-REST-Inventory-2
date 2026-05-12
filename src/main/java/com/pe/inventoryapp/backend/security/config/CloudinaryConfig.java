d23c3541c26563d960bae6d907b9240aa66a5507
ArmandoEnrique1010K <enrique1010k@gmail.com>
2026-05-12 18:38:11 -0500

    Update CloudinaryConfig.java

--- src/main/java/com/pe/inventoryapp/backend/security/config/CloudinaryConfig.java
+++ src/main/java/com/pe/inventoryapp/backend/security/config/CloudinaryConfig.java
@@ -1,13 +1,12 @@
 package com.pe.inventoryapp.backend.security.config;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 
 // import io.github.cdimascio.dotenv.Dotenv;
 
